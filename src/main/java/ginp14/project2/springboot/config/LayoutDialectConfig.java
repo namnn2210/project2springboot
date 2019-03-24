ab64b03872a9918bbc5a30df3df0bf4e5b4710ac
Ngô Ngọc Nam <namngocngo22@Ngos-MBP.net.fpt>
2019-03-24 17:54:47 +0700

    done config fragments

---
+++ src/main/java/ginp14/project2/springboot/config/LayoutDialectConfig.java
@@ -0,0 +1,13 @@
+package ginp14.project2.springboot.config;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LayoutDialectConfig {
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+}
--- src/main/java/ginp14/project2/springboot/config/WebConfig.java
+++ src/main/java/ginp14/project2/springboot/config/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -42,7 +43,7 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/css/**","/js/**","/fragments/**","/views/**").permitAll()
                 .antMatchers("/users/register").permitAll()
                 .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
                 .authenticated().and().csrf().disable().formLogin()
