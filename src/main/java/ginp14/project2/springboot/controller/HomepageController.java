4c36cf8e648b100cba741f0484f2e922def5b417
Ngô Ngọc Nam <namngocngo22@Ngos-MBP.net.fpt>
2019-03-24 18:08:26 +0700

    Done 2nd

--- src/main/java/ginp14/project2/springboot/controller/HomepageController.java
+++ src/main/java/ginp14/project2/springboot/controller/HomepageController.java
@@ -6,16 +6,13 @@ import ginp14.project2.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 
 
 @Controller
-@CrossOrigin(origins = "http://localhost:8080")
 public class HomepageController {
 
     @RequestMapping("/")
