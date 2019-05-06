fa15c2e23f20edf72ca17bbc9265db5dd464d91c
Ngô Ngọc Nam <ngongocnam@Ngos-MBP.net.fpt>
2019-05-06 23:50:56 +0700

    doing

--- src/main/java/ginp14/project2/springboot/controller/HomepageController.java
+++ src/main/java/ginp14/project2/springboot/controller/HomepageController.java
@@ -2,16 +2,12 @@ package ginp14.project2.springboot.controller;
 
 
 
-import ginp14.project2.springboot.entity.Movie;
+
 import ginp14.project2.springboot.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.security.Principal;
-import java.util.List;
 
 
 @Controller
@@ -25,4 +21,5 @@ public class HomepageController {
         model.addAttribute("movies",movieService.findAll());
         return "views/homepage";
     }
+
 }
--- src/main/java/ginp14/project2/springboot/controller/MovieController.java
+++ src/main/java/ginp14/project2/springboot/controller/MovieController.java
@@ -1,17 +1,25 @@
 package ginp14.project2.springboot.controller;
 
+import ginp14.project2.springboot.entity.Movie;
+import ginp14.project2.springboot.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/movies")
 public class MovieController {
 
+    @Autowired
+    private MovieService movieService;
 
     @GetMapping("/detail")
-    public String showMovieDetail() {
+    public String showMovieDetail(@RequestParam int movieId, Model model) {
+        Movie movie = movieService.findById(movieId);
+        model.addAttribute("movie",movie);
         return "views/movies/movie_detail";
     }
 }
